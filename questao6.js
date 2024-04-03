f67686bb9a0a3537e8e424c526217fe4c8021636
ietheuszx <fernandomaranss68@gmail.com>
2024-04-03 16:34:14 -0400

    Questão 6 resolvida

---
+++ questao6.js
@@ -0,0 +1,9 @@
+/*
+6. Questão: Escreva um código que imprima os números de 1 a 5 utilizando while.
+*/
+
+let resultado = 0;
+ while( resultado <= 5) {
+    console.log("o resultado é: " + resultado);
+    resultado ++
+ }
