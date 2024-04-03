a7e6fc5e959af57b4319c15852e74c5289126911
ietheuszx <fernandomaranss68@gmail.com>
2024-04-03 16:33:53 -0400

    Questão 3 resolvida

---
+++ questao3.js
@@ -0,0 +1,8 @@
+/*
+3. Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente.
+*/
+
+for(let dec = 10; dec >= 1; dec --) {
+    console.log("Estes são os números decrescentes: " + dec)
+}
